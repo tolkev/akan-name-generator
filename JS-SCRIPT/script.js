6791c651d088120e8c3c515b8ffbca1a447895dd
tolkev <51361914+tolkev@users.noreply.github.com>
2022-10-24 18:54:28 +0300

    create get gender functon

--- JS-SCRIPT/script.js
+++ JS-SCRIPT/script.js
@@ -3,3 +3,12 @@ function getAkanName () {
     let monthOfBirth = Number(document.getElementById("month-input").value);
     let dayOfBirth = Number(document.getElementById("day-input").value);
     let genders = document.getElementsByName("gender");
+
+    // function to get gender
+  function getGender () {
+    for (let gender of genders){
+      if (gender.checked){
+        return gender.value;
+      }
+    }
+  }
