1a36b5f10b57393bb48abcfcb020dc1939d3501c
tolkev <51361914+tolkev@users.noreply.github.com>
2022-10-24 18:53:14 +0300

    create getAkanName functon

---
+++ JS-SCRIPT/script.js
@@ -0,0 +1,5 @@
+function getAkanName () {
+    let yearOfBirth = document.getElementById("year-input").value;
+    let monthOfBirth = Number(document.getElementById("month-input").value);
+    let dayOfBirth = Number(document.getElementById("day-input").value);
+    let genders = document.getElementsByName("gender");
